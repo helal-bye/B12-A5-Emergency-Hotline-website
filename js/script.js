a507bad647b55b6965c73e74256b8c1d1a233e75
sudohelal <helaluddin40@outlook.com>
2025-08-27 15:11:02 +0600

    added clear button javascript and copy javascript button

--- js/script.js
+++ js/script.js
@@ -1,31 +1,37 @@
 const timeData = [];
-document.getElementById('emergency-number-container').addEventListener('click', function(event){
+document
+  .getElementById("emergency-number-container")
+  .addEventListener("click", function (event) {
     event.preventDefault();
-    if(event.target.className.includes('heart-btn')){
-        const heartCount = document.getElementById('heart-count').innerText;
-        const heartTotal = Number(heartCount) + 1;
-        document.getElementById('heart-count').innerText = heartTotal;
+    if (event.target.className.includes("heart-btn")) {
+      const heartCount = document.getElementById("heart-count").innerText;
+      const heartTotal = Number(heartCount) + 1;
+      document.getElementById("heart-count").innerText = heartTotal;
     }
-    if(event.target.className.includes('call-btn')){
-        const emergencyNumberBtn = event.target;
-        const cardTitle = event.target.parentNode.parentNode.childNodes[3].innerText;
-        const cardNumber = event.target.parentNode.parentNode.childNodes[7].innerText;
-        alert("Calling "+cardTitle+" "+cardNumber);
-        const coinCount = document.getElementById('coin-count').innerText;
-        const coinTotal = coinCount - 20;
-        document.getElementById('coin-count').innerText = coinTotal;
-        if(coinTotal < 0){
-            alert('You have not suficient balance, you must minimum 20 coin');
-            return;
-        }
-        const data = {
-            time : new Date().toLocaleTimeString(),
-        }
-        timeData.push(data);
-        
-        const callHistoryContainer = document.getElementById('call-history-container');
-        const div = document.createElement('div');
-        div.innerHTML = `
+    if (event.target.className.includes("call-btn")) {
+      const emergencyNumberBtn = event.target;
+      const cardTitle =
+        event.target.parentNode.parentNode.childNodes[3].innerText;
+      const cardNumber =
+        event.target.parentNode.parentNode.childNodes[7].innerText;
+      alert("Calling " + cardTitle + " " + cardNumber);
+      const coinCount = document.getElementById("coin-count").innerText;
+      const coinTotal = coinCount - 20;
+      document.getElementById("coin-count").innerText = coinTotal;
+      if (coinTotal < 0) {
+        alert("You have not suficient balance, you must have minimum 20 coin");
+        return;
+      }
+      const data = {
+        time: new Date().toLocaleTimeString(),
+      };
+      timeData.push(data);
+
+      const callHistoryContainer = document.getElementById(
+        "call-history-container"
+      );
+      const div = document.createElement("div");
+      div.innerHTML = `
         
         <div class="flex items-center justify-between p-4 bg-[#fafafa] mt-4 rounded-lg">
                         <div>
@@ -35,17 +41,28 @@ document.getElementById('emergency-number-container').addEventListener('click', 
                         <p class="text-[#111111] font-normal text-lg">${data.time}</p>
                     </div>
         
-        `
-        callHistoryContainer.appendChild(div);
-        
-
+        `;
+      callHistoryContainer.appendChild(div);
     }
 
-    if(event.target.className.includes('copy-btn')){
-        const cardNumber = event.target.parentNode.parentNode.childNodes[7].innerText;
-        alert("Number is copied "+cardNumber);
-        const copyCount = document.getElementById('copy-count').innerText;
-        const copyTotal = Number(copyCount) + 1;
-        document.getElementById('copy-count').innerText = copyTotal;
+    if (event.target.className.includes("copy-btn")) {
+      const cardNumber =
+        event.target.parentNode.parentNode.childNodes[7].innerText;
+
+      const copyCount = document.getElementById("copy-count").innerText;
+      const copyTotal = Number(copyCount) + 1;
+      document.getElementById("copy-count").innerText = copyTotal;
+      try {
+        navigator.clipboard.writeText(cardNumber);
+        alert("Content copied to clipboard " + cardNumber);
+      } catch (err) {
+        console.error("Failed to copy: ", err);
+      }
     }
+  });
+document.getElementById('clear-btn').addEventListener('click', function(event){
+    event.preventDefault();
+    const callHistoryContainer = document.getElementById("call-history-container");
+    callHistoryContainer.innerHTML = '';
+
 })
